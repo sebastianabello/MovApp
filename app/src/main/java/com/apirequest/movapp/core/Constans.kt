1ecba0cbf18ec7eb332ce6ae1d12aa7147537f40
Juan Abello <juanse.abello.2004@gmail.com>
2024-09-12 14:59:58 -0500

    New desing

--- app/src/main/java/com/apirequest/movapp/core/Constans.kt
+++ app/src/main/java/com/apirequest/movapp/core/Constans.kt
@@ -1,7 +1,7 @@
 package com.apirequest.movapp.core
 
 object Constans {
-    const val API_KEY = "api_key"
+    const val API_KEY = "api_k"
     const val BASE_URL = "https://api.themoviedb.org/3/movie/"
     const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500"
     const val IMAGE_WIDTH = 300
